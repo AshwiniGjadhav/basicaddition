c461a96a52ebd9e830219e5cc3f66ab06a375625
Dell <Dell@DESKTOP-LGPMCGR>
2022-09-09 00:11:19 +0530

    Add project files.

---
+++ basicaddition/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicaddition
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two numbers");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = n1 + n2;
+            Console.WriteLine($"Addition is {sum}");
+        }
+    }
+}
